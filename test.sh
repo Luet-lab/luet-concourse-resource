eab8b4af8903b9ccc3e16d0c54a2b87c2d6b71b6
Ettore Di Giacinto <mudler@sabayon.org>
2020-11-01 13:09:23 +0100

    Fixups

--- test.sh
+++ test.sh
@@ -15,3 +15,28 @@ echo "No output: $out"
 exit 1
 
 fi
+
+mkdir tmp
+out=`bash assets/in tmp <<EOF
+{
+  "source": {
+    "url": "https://get.mocaccino.org/mocaccino-extra"
+  },
+  "version": { "ref": "MISSING" }
+}
+EOF
+`
+
+echo $out
+if [ -z "$out" ]; then
+echo "No output: $out"
+exit 1
+
+fi
+
+if [ ! -e "tmp/repository.yaml" ]; then
+echo "repository file not fetched"
+exit 1
+fi
+
+rm -rfv tmp/repository.yaml
